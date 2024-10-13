6783ab97bded6d6faf887696d2a42c50758a5f71
nikosafa <nikosafa7895@gmail.com>
2024-10-13 10:06:36 +0200

    Nye noter

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+//Note links:
 // https://www.youtube.com/watch?v=JYgKmCIB53c
 //https://www.geeksforgeeks.org/java-swing-jtogglebutton-class/
+//https://www.w3schools.com/jsref/met_element_addeventlistener.asp
 
-// DOM elements
+// DOM elementer
 const lightModeToggle = document.getElementById('lightModeToggle');
 const body = document.body;
-const header = document.querySelector('header'); // Select the header element
+const header = document.querySelector('header'); // Vælger header elementet
 
-// Add event listener to the button
+// Tilføjer event listener til knappen
 lightModeToggle.addEventListener('click', () => {
-    // Toggle the light mode class on both body and header
+    // Skifer knappens lys tilstand på både body og header
     body.classList.toggle('light-mode');
-    header.classList.toggle('light-mode'); // Add this line to toggle header styles
+    header.classList.toggle('light-mode');
 
-    // Update the toggle button text based on the current mode
+    // Opdater knappens tekst baseret på den aktuelle tilstand
     if (body.classList.contains('light-mode')) {
         lightModeToggle.textContent = 'Dark Mode';
     } else {
